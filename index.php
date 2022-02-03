d01b70aa32568437a1da46af14ba35cadf3634ec
Mughis Fadhil <mughisfar@gmail.com>
2022-02-03 12:05:09 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>hello dunia</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga consequatur ratione dolorem unde, dignissimos sit assumenda labore obcaecati amet placeat eaque libero enim sunt tempore perferendis voluptate incidunt veritatis saepe?</p>
 </body>
 </html>
